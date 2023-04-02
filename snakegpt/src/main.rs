20858ee682a3c6849c85a4add414a404fda0cee3
Corey Alexander <coreyja@gmail.com>
2023-04-02 14:25:46 -0400

    Try to load the extensions, the loading C stuff works, but the specific vector0 extension gives a SegFault

--- snakegpt/src/main.rs
+++ snakegpt/src/main.rs
@@ -1,4 +1,5 @@
 use miette::{IntoDiagnostic, Result};
+use rusqlite::Connection;
 
 use crate::openai::{
     embeddings::{EmbeddingResponse, EmbeddingsRequest},
@@ -11,13 +12,40 @@ static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_P
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let config = Config::from_env()?;
+    // let config = Config::from_env()?;
 
-    let client = config.client()?;
+    // let client = config.client()?;
 
-    let response_body = client.embeddings("Hello world!").await?;
+    // let response_body = client.embeddings("Hello world!").await?;
 
-    dbg!(&response_body);
+    // dbg!(&response_body);
+
+    // Ok(())
+
+    let conn = Connection::open_in_memory().into_diagnostic()?;
+
+    load_my_extension(&conn)?;
+
+    conn.query_row("select sqlite_version()", (), |result| {
+        dbg!(&result);
+        Ok(())
+    })
+    .into_diagnostic()?;
 
     Ok(())
+}
+
+fn load_my_extension(conn: &Connection) -> Result<()> {
+    // Safety: We fully trust the loaded extension and execute no untrusted SQL
+    // while extension loading is enabled.
+    unsafe {
+        conn.load_extension_enable().into_diagnostic()?;
+        conn.load_extension("./vendor/vector0", None)
+            .into_diagnostic()?;
+        conn.load_extension("./vendor/vss0", None)
+            .into_diagnostic()?;
+        conn.load_extension_disable().into_diagnostic()?;
+
+        Ok(())
+    }
 }
