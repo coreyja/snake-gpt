6b49134174157cdd800ba87186787f495c9a8ebc
Corey Alexander <coreyja@gmail.com>
2023-04-16 14:15:08 -0400

    Our server can response with hard coded values now

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,38 @@
-use axum::{routing::get, Router};
+use axum::{
+    http::Method,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use tower_http::cors::{Any, CorsLayer};
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct AnswerResp {
+    answer: String,
+    question: String,
+}
 
 #[tokio::main]
 async fn main() {
+    let cors = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST])
+        // allow requests from any origin
+        .allow_origin(Any);
+
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route(
+            "/api/v0/chat",
+            post(|| async {
+                Json(AnswerResp {
+                    answer: "Hello, World!".to_owned(),
+                    question: "Hello, World!".to_owned(),
+                })
+            }),
+        )
+        .layer(cors);
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
